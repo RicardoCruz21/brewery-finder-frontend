cc3e7e2ebd231894173514f6b994f8ac5b848131
RicardoCruz21 <abstergo21@gmail.com>
2022-12-19 22:03:58 -0600

    config env var for prod and dev

--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router/index'
-import store from './store/index'
-import axios from 'axios'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router/index";
+import store from "./store/index";
+import axios from "axios";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-axios.defaults.baseURL = process.env.VUE_APP_REMOTE_API;
+axios.defaults.baseURL =
+  process.env.VUE_APP_REMOTE_API_PROD || process.env.VUE_APP_REMOTE_API_DEV;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
